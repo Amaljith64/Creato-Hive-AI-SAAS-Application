4ee6222265d9f6bce357b825b757cc15dd2ee27d
Amaljith64 <amaljith64@gmail.com>
2023-08-11 23:12:45 +0530

    Landing Page UI done

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,19 +1,15 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
+import { LandingNavbar } from "@/components/landing-navbar";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingContent } from "@/components/landing-content";
 
-export default function Home() {
-  return (
-    <div>
-      Landing Page(unprotected)
-      <div>
-        <Link href="/sign-in">
-          <Button>Login</Button>
-        </Link>
-        <Link href="/sign-up">
-          <Button>Register</Button>
-        </Link>
-      </div>
+const LandingPage = () => {
+  return ( 
+    <div className="h-full ">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
     </div>
-  );
+   );
 }
+ 
+export default LandingPage;
